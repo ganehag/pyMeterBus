baf4761b6b456eeb71d918eb72cfde1bf19861ba
Mikael Ganehag Brorsson <mikael.brorsson@gmail.com>
2014-10-01 11:03:10 +0200

    pep8

--- mbus/telegram_variable_data_record.py
+++ mbus/telegram_variable_data_record.py
@@ -1,75 +1,91 @@
+import json
 from dif_telegram_field import DIFTelegramField
 from vif_telegram_field import VIFTelegramField
 from telegram_data_field import TelegramDataField
 
+
 class TelegramVariableDataRecord(object):
-	def __init__(self):
-		self._dif       = DIFTelegramField()
-		self._difes     = []
+    def __init__(self):
+        self._dif = DIFTelegramField()
+        self._difes = []
 
-		self._vif       = VIFTelegramField()
-		self._vifes     = []
+        self._vif = VIFTelegramField()
+        self._vif.parent = self
+        self._vifes = []
 
-		self._dataField = TelegramDataField()
+        self._dataField = TelegramDataField()
+        self._dataField.parent = self
 
-	def parse(self):
-		self.dif = DIFTelegramField()
-		self.dif.parse()
-		self.vif = VIFTelegramField()
-		self.vif.setParent(self)
+    def parse(self):
+        self.dif = DIFTelegramField()
+        self.dif.parse()
+        self.vif = VIFTelegramField()
+        self.vif.parent = self
 
-		self.dataField = TelegramDataField()
-		self.dataField.setParent(self)
-		self.dataField.parse()
+        self.dataField = TelegramDataField()
+        self.dataField.parent = self
+        self.dataField.parse()
 
-	@property
-	def dif(self):
-	    return self._dif
-	@dif.setter
-	def dif(self, value):
-	    self._dif = value
+    @property
+    def dif(self):
+        return self._dif
 
-	@property
-	def vif(self):
-	    return self._vif
-	@vif.setter
-	def vif(self, value):
-	    self._vif = value
+    @dif.setter
+    def dif(self, value):
+        self._dif = value
 
-	@property
-	def difes(self):
-	    return self._difes
-	@difes.setter
-	def difes(self, value):
-	    self._difes = value
-	
-	@property
-	def vifes(self):
-	    return self._vifes
-	@vifes.setter
-	def vifes(self, value):
-	    self._vifes = value
-	
-	@property
-	def dataField(self):
-	    return self._dataField
-	@dataField.setter
-	def dataField(self, value):
-	    self._dataField = value
+    @property
+    def vif(self):
+        return self._vif
 
-	def debug(self):
-		print "VARIABLE DATA RECORD:"
+    @vif.setter
+    def vif(self, value):
+        self._vif = value
 
-		self.dif.debug()
+    @property
+    def difes(self):
+        return self._difes
 
-		for item in self.difes:
-			item.debug()
+    @difes.setter
+    def difes(self, value):
+        self._difes = value
 
-		self.vif.debug()
+    @property
+    def vifes(self):
+        return self._vifes
 
-		for item in self.vifes:
-			item.debug()
+    @vifes.setter
+    def vifes(self, value):
+        self._vifes = value
 
-		self.dataField.debug()
+    @property
+    def dataField(self):
+        return self._dataField
 
-		print "=================================================="
+    @dataField.setter
+    def dataField(self, value):
+        self._dataField = value
+
+    def debug(self):
+        print "VARIABLE DATA RECORD:"
+
+        self.dif.debug()
+
+        for item in self.difes:
+            item.debug()
+
+        self.vif.debug()
+
+        for item in self.vifes:
+            item.debug()
+
+        self.dataField.debug()
+
+        print "======================="
+
+    def to_JSON(self):
+        return json.dumps({
+            'value': self.dataField.parsed_value,
+            'unit': self.dataField.parent.vif.m_unit.name,
+            'type': str(self.dataField.parent.vif.type)
+        })
