a7620728c03bc770f9fe021d304d75f6a2bb6328
Mikael Ganehag Brorsson <mikael.brorsson@gmail.com>
2017-05-10 21:16:30 +0200

    Support for sending 'select' frames

--- meterbus/serial.py
+++ meterbus/serial.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from .telegram_short import TelegramShort
+from .telegram_long import TelegramLong
 from .aux import is_primary_address, is_secondary_address
 from .defines import *
 
@@ -28,6 +29,40 @@ def send_request_frame(ser, address):
   frame.header.aField.parts = [address]
 
   ser.write(frame)
+
+def send_select_frame(ser, secondary_address):
+  frame = TelegramLong()
+
+  frame.header.cField.parts = [
+    CONTROL_MASK_SND_UD | CONTROL_MASK_DIR_M2S | CONTROL_MASK_FCB
+  ]
+  frame.header.aField.parts = [ADDRESS_NETWORK_LAYER]
+
+  frame.body.bodyHeaderLength = 9
+  frame_data = [
+    CONTROL_INFO_SELECT_SLAVE,
+    0, 0, 0, 0,
+    0, 0, 0, 0
+  ]
+  val = int(secondary_address[14:], 16)
+  frame_data[8] = val & 0xFF
+
+  val = int(secondary_address[12:14], 16)
+  frame_data[7] = val & 0xFF
+
+  val = int(secondary_address[8:12], 16)
+  frame_data[5] = (val>>8) & 0xFF
+  frame_data[6] = val & 0xFF
+
+  frame_data[4] = int(secondary_address[0:2], 16)
+  frame_data[3] = int(secondary_address[2:4], 16)
+  frame_data[2] = int(secondary_address[4:6], 16)
+  frame_data[1] = int(secondary_address[6:8], 16)
+
+  frame.body.bodyHeader = frame_data
+
+  ser.write(frame)
+
 
 def recv_frame(ser, length=1):
   return ser.read(length)
