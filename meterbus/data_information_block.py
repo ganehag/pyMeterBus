f30b449351a7873b1a11739d43661c629b4ed49c
Mikael Ganehag Brorsson <mikael.brorsson@gmail.com>
2017-05-18 21:46:35 +0200

    Added comments

--- meterbus/data_information_block.py
+++ meterbus/data_information_block.py
@@ -3,10 +3,10 @@ from .telegram_field import TelegramField
 
 
 class DataInformationBlock(TelegramField):
-    EXTENSION_BIT_MASK = 0x80      # 1000 0000
-    FUNCTION_MASK = 0x30           # 0011 0000
-    DATA_FIELD_MASK = 0x0F         # 0000 1111
-    MORE_RECORDS_FOLLOW = 0x1F     # 0001 1111
+    EXTENSION_BIT_MASK = 0x80  # 1000 0000
+    FUNCTION_MASK = 0x30   # 0011 0000
+    DATA_FIELD_MASK = 0x0F  # 0000 1111
+    MORE_RECORDS_FOLLOW = 0x1F  # 0001 1111
 
     def __init__(self, parts=None):
         super(DataInformationBlock, self).__init__(parts)
@@ -64,24 +64,34 @@ class DataInformationBlock(TelegramField):
             (self.parts[0] & self.FUNCTION_MASK) >> 4)
 
     @property
+    def is_variable_length(self):
+        try:
+            dif = self.parts[0]
+            return (dif & self.DATA_FIELD_MASK) == 0x0D
+        except IndexError:
+            pass
+
+        return False
+
+    @property
     def length_encoding(self):
         len_enc = self.parts[0] & self.DATA_FIELD_MASK
 
         return {
             0: (0, DataEncoding.ENCODING_NULL),
-            1: (len_enc, DataEncoding.ENCODING_INTEGER),
-            2: (len_enc, DataEncoding.ENCODING_INTEGER),
-            3: (len_enc, DataEncoding.ENCODING_INTEGER),
-            4: (len_enc, DataEncoding.ENCODING_INTEGER),
-            5: (4, DataEncoding.ENCODING_REAL),
-            6: (6, DataEncoding.ENCODING_INTEGER),
-            7: (8, DataEncoding.ENCODING_INTEGER),
-            8: (0, DataEncoding.ENCODING_NULL),
-            9: (len_enc - 8, DataEncoding.ENCODING_BCD),
-            10: (len_enc - 8, DataEncoding.ENCODING_BCD),
-            11: (len_enc - 8, DataEncoding.ENCODING_BCD),
-            12: (len_enc - 8, DataEncoding.ENCODING_BCD),
-            13: (6, DataEncoding.ENCODING_VARIABLE_LENGTH),
-            14: (6, DataEncoding.ENCODING_BCD),
-            15: (0, DataEncoding.ENCODING_NULL)  # Not right FIXME
+            1: (len_enc, DataEncoding.ENCODING_INTEGER),  # 1 byte int [8 bit]
+            2: (len_enc, DataEncoding.ENCODING_INTEGER),  # 2 byte int [16 bit] or date...
+            3: (len_enc, DataEncoding.ENCODING_INTEGER),  # 3 byte int [24 bit]
+            4: (len_enc, DataEncoding.ENCODING_INTEGER),  # 4 byte int [32 bit] or date...
+            5: (4, DataEncoding.ENCODING_REAL),  # 4 byte float [32 bit]
+            6: (6, DataEncoding.ENCODING_INTEGER),  # 6 byte int [48 bit]
+            7: (8, DataEncoding.ENCODING_INTEGER),  # 8 byte int [64 bit]
+            8: (0, DataEncoding.ENCODING_NULL),  # Nothing...
+            9: (len_enc - 8, DataEncoding.ENCODING_BCD),  # 2 digit BCD [8 bit]
+            10: (len_enc - 8, DataEncoding.ENCODING_BCD),  # 4 digit BCD [16 bit]
+            11: (len_enc - 8, DataEncoding.ENCODING_BCD),  # 6 digit BCD [24 bit]
+            12: (len_enc - 8, DataEncoding.ENCODING_BCD),  # 8 digit BCD [32 bit]
+            13: (6, DataEncoding.ENCODING_VARIABLE_LENGTH),  # variable length
+            14: (6, DataEncoding.ENCODING_BCD),  # 12 digit BCD [40 bit]
+            15: (0, DataEncoding.ENCODING_NULL)  # Special Function FIXME
         }[len_enc]
