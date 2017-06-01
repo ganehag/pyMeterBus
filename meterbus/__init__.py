838076d18e49d73b58eae2fc58f2c7fba66d0798
Mikael Ganehag Brorsson <mikael.brorsson@gmail.com>
2017-06-01 14:11:39 +0200

    Expose DataInformationBlock

--- meterbus/__init__.py
+++ meterbus/__init__.py
@@ -21,6 +21,7 @@ from .telegram_short import TelegramShort
 from .telegram_control import TelegramControl
 from .telegram_long import TelegramLong
 
+from .data_information_block import DataInformationBlock
 from .telegram_header import TelegramHeader
 from .telegram_body import TelegramBody, TelegramBodyHeader, \
     TelegramBodyPayload
