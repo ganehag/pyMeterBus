1ac16a3bdf55135f6a48cbfd51ab1ec364d83fcb
Mikael Ganehag Brorsson <mikael.brorsson@gmail.com>
2017-06-01 20:32:32 +0200

    Added tests for globals

---
+++ tests/test_globals.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+
+import unittest
+import meterbus
+from meterbus.exceptions import *
+
+
+class TestSequenceFunctions(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_debug_default_value(self):
+        self.assertEqual(meterbus.g.debug, False)
+
+    def test_debug_set_true(self):
+        meterbus.g.debug = True
+        self.assertEqual(meterbus.g.debug, True)
+
+if __name__ == '__main__':
+    unittest.main()
