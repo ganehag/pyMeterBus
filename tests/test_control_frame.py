ec4a599ac4d82298caf77fdfc59a0e60488d9e85
Mikael Ganehag Brorsson <mikael.brorsson@gmail.com>
2017-06-05 16:42:48 +0200

    Added control telegram test

---
+++ tests/test_control_frame.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+
+import unittest
+import meterbus
+from meterbus.exceptions import *
+
+
+class TestSequenceFunctions(unittest.TestCase):
+    def setUp(self):
+        self.frame = "\x68\x03\x03\x68\x08\x0b\x72\x85\x16"
+
+    def test_load_control_frame_str(self):
+        frame = meterbus.TelegramControl(self.frame)
+        self.assertIsInstance(frame, meterbus.TelegramControl)
+
+    def test_frame_header_setter(self):
+        frame = meterbus.TelegramControl(self.frame)
+        hdr = meterbus.TelegramHeader()
+        frame.header = hdr
+        self.assertIs(frame.header, hdr)
+
+    def test_frame_body_setter(self):
+        frame = meterbus.TelegramControl(self.frame)
+        body = meterbus.TelegramBody()
+        frame.body = body
+        self.assertIs(frame.body, body)
+
+if __name__ == '__main__':
+    unittest.main()
