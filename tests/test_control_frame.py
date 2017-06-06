7e286d894fb2b5c84e9814f99f2a60adc98b98e8
Mikael Ganehag Brorsson <mikael.brorsson@gmail.com>
2017-06-06 17:02:11 +0200

    Fixed for py2<->py3

--- tests/test_control_frame.py
+++ tests/test_control_frame.py
@@ -10,6 +10,11 @@ import unittest
 import meterbus
 from meterbus.exceptions import *
 
+def char2int(val):
+    if type(val) == int:
+        return val
+    return ord(val)
+
 
 class TestSequenceFunctions(unittest.TestCase):
     def setUp(self):
@@ -18,11 +23,11 @@ class TestSequenceFunctions(unittest.TestCase):
 
     def test_load_control_frame_str(self):
         frame = meterbus.TelegramControl(self.frame)
-        self.assertEqual(list(frame), list(self.frame_bytes))
+        self.assertEqual(list(frame), list(map(char2int, list(self.frame_bytes))))
 
     def test_load_control_frame_bytes(self):
         frame = meterbus.TelegramControl(self.frame_bytes)
-        self.assertEqual(list(frame), list(self.frame_bytes))
+        self.assertEqual(list(frame), list(map(char2int, list(self.frame_bytes))))
 
     def test_frame_header_setter(self):
         frame = meterbus.TelegramControl(self.frame)
--- tests/test_short_frame.py
+++ tests/test_short_frame.py
@@ -10,6 +10,10 @@ import unittest
 import meterbus
 from meterbus.exceptions import *
 
+def char2int(val):
+    if type(val) == int:
+        return val
+    return ord(val)
 
 class TestSequenceFunctions(unittest.TestCase):
     def setUp(self):
@@ -18,11 +22,13 @@ class TestSequenceFunctions(unittest.TestCase):
 
     def test_load_string(self):
         frame = meterbus.TelegramShort(self.frame)
-        self.assertEqual(list(frame), list(self.frame_bytes))
+        frame_bytes = list(map(char2int, list(self.frame_bytes)))
+        self.assertEqual(list(frame), frame_bytes)
 
     def test_load_bytes(self):
         frame = meterbus.TelegramShort(self.frame_bytes)
-        self.assertEqual(list(frame), list(self.frame_bytes))
+        frame_bytes = list(map(char2int, list(self.frame_bytes)))
+        self.assertEqual(list(frame), frame_bytes)
 
     def test_load_crc_error(self):
         frame_data = list(self.frame)
