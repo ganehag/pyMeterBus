c3437adc056bf2b13346ad78d9bcfd5f3cdde482
Mikael Ganehag Brorsson <mikael.brorsson@gmail.com>
2018-09-24 11:24:30 +0200

    Changed to pycryptodome

--- setup.py
+++ setup.py
@@ -1,10 +1,38 @@
 from setuptools import setup, find_packages
 
-setup(name="meterbus",
-      version="0.1dev",
-      description="Python implementation of the Meter-Bus protocol",
-      author="Mikael Ganehag Brorsson",
-      author_email="mikael.brorsson@gmail.com",
-      packages=find_packages(exclude=['tests', 'mbus_ref']),
-      install_requires=["enum34", "simplejson", "pyaml", "pyserial"],
+with open('README.md') as f:
+    readme = f.read()
+
+with open('VERSION') as f:
+    version = f.readline()
+
+with open('LICENSE') as f:
+    license = f.readline()
+
+setup(name='pyMeterBus',
+      description='Python implementation of the Meter-Bus protocol',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      version=version,
+      url='https://github.com/ganehag/pyMeterBus',
+      author='Mikael Ganehag Brorsson',
+      author_email='mikael.brorsson@gmail.com',
+      license=license,
+
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development :: Libraries'
+      ],
+
+#      packages=find_packages(exclude=['tests', 'mbus_ref']),
+      packages=['meterbus'],
+      install_requires=['enum34', 'simplejson', 'pyaml',
+                        'pyserial', 'future', 'pycryptodome'],
 )
