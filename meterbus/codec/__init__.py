200666c2230a3bcf05ea73eae505bb4685a10359
Mikael Ganehag Brorsson <mikael.brorsson@gmail.com>
2026-05-12 09:54:02 +0200

    Export VIF parser

--- meterbus/codec/__init__.py
+++ meterbus/codec/__init__.py
@@ -4,6 +4,7 @@ from .crc import checksum
 from .dif import DataInformationParseError, DataInformationParseResult, parse_dif
 from .frame_decoder import FrameDecodeResult, FrameDecoder, decode_frame
 from .telegram_decoder import TelegramDecoder, decode_telegram, decode_variable_data_header
+from .vif import ValueInformationParseError, ValueInformationParseResult, parse_vif
 
 __all__ = [
     "DataInformationParseError",
@@ -11,9 +12,12 @@ __all__ = [
     "FrameDecodeResult",
     "FrameDecoder",
     "TelegramDecoder",
+    "ValueInformationParseError",
+    "ValueInformationParseResult",
     "checksum",
     "decode_frame",
     "decode_telegram",
     "decode_variable_data_header",
     "parse_dif",
+    "parse_vif",
 ]
