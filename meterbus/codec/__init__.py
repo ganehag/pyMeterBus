2b878ecd4eb400704b51edaeb3fb6d0c5792df94
Mikael Ganehag Brorsson <mikael.brorsson@gmail.com>
2026-05-12 09:28:28 +0200

    Export v2 telegram decoder

--- meterbus/codec/__init__.py
+++ meterbus/codec/__init__.py
@@ -2,10 +2,14 @@
 
 from .crc import checksum
 from .frame_decoder import FrameDecodeResult, FrameDecoder, decode_frame
+from .telegram_decoder import TelegramDecoder, decode_telegram, decode_variable_data_header
 
 __all__ = [
     "FrameDecodeResult",
     "FrameDecoder",
+    "TelegramDecoder",
     "checksum",
     "decode_frame",
+    "decode_telegram",
+    "decode_variable_data_header",
 ]
