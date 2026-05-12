024ef595088a6e325a148858a9198fced86d845a
Mikael Ganehag Brorsson <mikael.brorsson@gmail.com>
2026-05-12 10:02:28 +0200

    Export value decoder

--- meterbus/codec/__init__.py
+++ meterbus/codec/__init__.py
@@ -4,6 +4,7 @@ from .crc import checksum
 from .dif import DataInformationParseError, DataInformationParseResult, parse_dif
 from .frame_decoder import FrameDecodeResult, FrameDecoder, decode_frame
 from .telegram_decoder import TelegramDecoder, decode_telegram, decode_variable_data_header
+from .value import ValueDecodeError, ValueDecodeResult, decode_value
 from .vif import ValueInformationParseError, ValueInformationParseResult, parse_vif
 
 __all__ = [
@@ -12,11 +13,14 @@ __all__ = [
     "FrameDecodeResult",
     "FrameDecoder",
     "TelegramDecoder",
+    "ValueDecodeError",
+    "ValueDecodeResult",
     "ValueInformationParseError",
     "ValueInformationParseResult",
     "checksum",
     "decode_frame",
     "decode_telegram",
+    "decode_value",
     "decode_variable_data_header",
     "parse_dif",
     "parse_vif",
