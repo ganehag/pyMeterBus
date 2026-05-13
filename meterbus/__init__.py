7715fe111b05365dcea059c5ff2df397385d5b31
Mikael Ganehag Brorsson <mikael.brorsson@gmail.com>
2026-05-13 13:27:31 +0200

    Bump version to 2.0.0a2

--- meterbus/__init__.py
+++ meterbus/__init__.py
@@ -6,7 +6,7 @@ from .api import decode, decode_one, decode_one_frame
 
 __author__ = "Mikael Ganehag Brorsson"
 __license__ = "BSD-3-Clause"
-__version__ = "2.0.0a1"
+__version__ = "2.0.0a2"
 
 __all__ = [
     "decode",
